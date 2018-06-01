cf3cdb37f828490a3de640f1433975b285ef7720
karenzshea <karenzshea@gmail.com>
2018-06-01 02:45:30 +0200

    move ctors into cpp, define tile idx building

--- tile_index.cpp
+++ tile_index.cpp
@@ -13,6 +13,62 @@ extern "C" {
 
 namespace hite
 {
+    // Q should this use an int based Coordinate type?
+    Coordinate TileIndex::parseCoordFromName(const char* fileName)
+    {
+        std::regex re{"^([NWSE]{1})([0-9]{2})([NWSE]{1})([0-9]{3})\\.hgt"};
+        std::cmatch out;
+        int x,y;
+        Coordinate parsed;
+        if (std::regex_match(fileName, out, re))
+        {
+            x = out[1] == 'N' ? std::stoi(out[2]) : std::stoi(out[2]) * -1;
+            y = out[3] == 'W' ? std::stoi(out[4]) : std::stoi(out[4]) * -1;
+            parsed.Longitude = x;
+            parsed.Latitude = y;
+        }
+        return parsed;
+    }
+    int TileIndex::normalizeCoordToIndex(const Coordinate &coordinate)
+    {
+        int offset_x = coordinate.Longitude + 180;
+        int offset_y = coordinate.Latitude + 90;
+        return offset_x * offset_y;
+    }
+    Elevation TileIndex::Lookup(const Coordinate &coordinate)
+    {
+        // initalized Tile validation
+    }
+    TileIndex::TileIndex(const char* dirpath, const std::vector<const char*> &files)
+    {
+        std::uint8_t x,y;
+        std::size_t i = 0;
+        for (; i < files.size(); i++)
+        {
+            std::regex re{"^.+([0-9]{2}).+([0-9]{3})\\.hgt"};
+            std::cmatch out;
+            if (std::regex_match(files[i], out, re))
+            {
+                if (out.size() != 3) throw std::runtime_error("Could not parse file name");
+
+                x = std::stoi(out[1]);
+                y = std::stoi(out[2]);
+            }
+            else
+            {
+                throw std::runtime_error("Could not parse file name");
+            }
+            // full path of file
+            char fullpath[strlen(dirpath) + 1 + strlen(files[i])];
+            strcpy(fullpath, dirpath);
+            strcat(fullpath, "/");
+            strcat(fullpath, files[i]);
+
+            Coordinate file_coord = parseCoordFromName(files[i]);
+            int coord_index = normalizeCoordToIndex(file_coord);
+            tiles[coord_index] = ElevationTile(x, y, fullpath);
+        }
+    }
     void readFileDir(const char* dirpath, std::vector<const char*> &files)
     {
         // save filenames of all .hgt files in a given directory
--- tile_index.hpp
+++ tile_index.hpp
@@ -1,46 +1,21 @@
 #include <vector>
+#include <array>
 #include "tile.hpp"
+#include "constants.hpp"
 
 namespace hite
 {
-    constexpr const int TILE_INDEX_SIZE = 180 * 360;
     struct TileIndex {
-        TileIndex(const char* dirpath, std::vector<const char*> &files)
-        {
-            std::uint8_t i = 0;
-            std::uint8_t x,y;
-            for (; i < files.size(); i++)
-            {
-                std::regex re{"^.+([0-9]{2}).+([0-9]{3})\\.hgt"};
-                std::cmatch out;
-                if (std::regex_match(files[i], out, re))
-                {
-                    if (out.size() != 3) throw std::runtime_error("Could not parse file name");
-
-                    x = std::stoi(out[1]);
-                    y = std::stoi(out[2]);
-                }
-                else
-                {
-                    throw std::runtime_error("Could not parse file name");
-                }
-                // full path of file
-                char fullpath[strlen(dirpath) + 1 + strlen(files[i])];
-                strcpy(fullpath, dirpath);
-                strcat(fullpath, "/");
-                strcat(fullpath, files[i]);
-
-                Coordinate file_coord = parseCoordFromName(files[i]);
-                int coord_index = normalizeCoordToIndex(file_coord);
-                tiles[coord_index] = ElevationTile(x, y, fullpath);
-            }
-        }
-        ElevationTile* lookup(Coordinate &coordinate);
+        // Q - does default dtor call dtor of ElevationTiles in `tiles` ?
+        TileIndex();
+        TileIndex(const char* dirpath, const std::vector<const char*> &files);
+        Elevation Lookup(const Coordinate &coordinate);
 
         private:
         Coordinate parseCoordFromName(const char* fileName);
-        int normalizeCoordToIndex(Coordinate coordinate);
-        std::vector<ElevationTile> tiles{TILE_INDEX_SIZE};
+        int normalizeCoordToIndex(const Coordinate &coordinate);
+
+        std::array<ElevationTile, TILE_INDEX_SIZE> tiles;
     };
     void loadTileIndex(std::vector<const char*> &paths, std::vector<ElevationTile> &tiles);
     void readFileDir(const char* dirpath, std::vector<const char*> &files);
