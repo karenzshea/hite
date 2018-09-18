45570ee87e789d0b861c829f3c6d95f780e3a73c
karenzshea <karenzshea@gmail.com>
2018-09-18 15:15:05 +0200

    hardcode - working with 30m data

--- constants.hpp
+++ constants.hpp
@@ -12,7 +12,7 @@ constexpr const std::int16_t MAX_ELEVATION = std::numeric_limits<std::int16_t>::
 constexpr const int MAX_INT = std::numeric_limits<int>::max();
 constexpr const double MAX_DOUBLE = std::numeric_limits<double>::max();
 
-constexpr const int MAX_TILE_SIZE = 3601;
+constexpr const int MAX_TILE_SIZE = 1201;
 constexpr const int TILE_INDEX_SIZE = 180 * 360;
 constexpr const int HGT_FILE_SIZE = 25934402;
 }
